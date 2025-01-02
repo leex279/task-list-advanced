86890c7052390b1664cf79c5c844f49bb6196ba2
leex <thomas@thirty3.de>
2025-01-02 01:29:48 +0100

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_DEV_MODE': false
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_DEV_MODE': env.VITE_DEV_MODE
+    }
+  };
 });
